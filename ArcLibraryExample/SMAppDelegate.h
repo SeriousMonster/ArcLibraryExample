dffd79ed3901984307210feee478f63deb3d97f2
Duane Fields <duane@duanefields.com>
2012-01-11 08:12:55 -0600

    Initial Commit

---
+++ ArcLibraryExample/SMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMAppDelegate.h
+//  ArcLibraryExample
+//
+//  Created by Duane Fields on 1/11/12.
+//  Copyright (c) 2012 Fast Wombat, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ArcLibraryExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ArcLibraryExample
+//
+//  Created by Duane Fields on 1/11/12.
+//  Copyright (c) 2012 Fast Wombat, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMAppDelegate class]));
+    }
+}
